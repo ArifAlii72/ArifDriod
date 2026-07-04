212fb278b262836e091f2aad11c510a6ab073524
ArifAlii72 <draliashiq@gmail.com>
2026-07-04 19:47:52 +0500

    Update MainActivity.kt

--- app/src/main/java/com/arifali/arifdroid/MainActivity.kt
+++ app/src/main/java/com/arifali/arifdroid/MainActivity.kt
@@ -1,1 +1,17 @@
 
+package com.arifali.arifdroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<CardView>(R.id.card_donate).setOnClickListener {
+            // Buy Me a Coffee link
+        }
+    }
+}
